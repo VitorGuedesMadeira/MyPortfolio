ffddc595d1d59a904ee47e0bb1547dbff6b96220
Vitor Guedes <vgm_rox@hotmail.com>
2022-06-13 20:44:34 -0300

    Fix linters

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
+const nav = document.querySelector('.menu');
+const menubuttom = document.querySelector('#menubuttom');
+
 function toggleMenu() {
-  const nav = document.querySelector('.menu');
-  const menubuttom = document.querySelector('#menubuttom');
   nav.classList.toggle('active');
   menubuttom.classList.toggle('active');
 }
 
 function closeMenu() {
-  const nav = document.querySelector('.menu');
   nav.classList.remove('active');
 }
