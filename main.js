53bc2209daac1d2ba16ee2e2b1a4b89a089b48fc
vanessaBonito <vanebonito@hotmail.com>
2022-06-13 12:35:25 -0500

    Add responsive menu

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ function toggleMenu() {
     const nav = document.querySelector('.menu')
     nav.classList.toggle('active')
 }
+
+function closeMenu(){
+    const nav = document.querySelector('.menu')
+    nav.classList.remove('active')
+}
