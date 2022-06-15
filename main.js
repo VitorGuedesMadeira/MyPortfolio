bd9e0028ff643eca0a7342f3b9383c0568ba506b
Vitor Guedes <vgm_rox@hotmail.com>
2022-06-15 16:20:26 -0300

    Work on modals

--- main.js
+++ main.js
@@ -15,9 +15,7 @@ menubutton.onclick = function () { toggleMenu(); };
 iconsButtons.onclick = function () { closeMenu(); };
 
 //-------------------Modal functions OPEN/CLOSE-----------------//
-
-//const mainProjButtom = document.querySelector('#mainprojectbuttom');
-const closureButtom = document.querySelectorAll('.close-buttom');
+const closeButtom = document.querySelectorAll('#x-button');
 
 function toggleModal(ID) {
   const modal = document.getElementById(ID);
@@ -29,15 +27,14 @@ function toggleModal(ID) {
     }
   }
 
-  closureButtom.onclick = function (ID) {
+  closeButtom.onclick = function () {
     modal.classList.remove('showUp');
   }
 }
 
-//mainProjButtom.addEventListener('click', () => toggleModal('modal-containerID0'));
-
 //-------------------DATA STORAGE (array of objects)------------------------//
-//CARDS
+
+//CARDS DATA
 const projects = [
   {
     projectID:0,
@@ -53,7 +50,7 @@ const projects = [
   {
     projectID:1,
     name: 'Proffesional art printing data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard.',
     featuredImageBig: 'images/firstprojectbig.png',
     featuredImageSmall: 'images/backgroundprojects2.png',
     technologies:['html', 'bootstrap', 'Ruby'],
@@ -63,7 +60,7 @@ const projects = [
   },
   {
     projectID:2,
-    name: 'Proffesional art printing data',
+    name: 'Data Dashboard Healthcare',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     featuredImage: 'images/firstprojectbig.png',
     technologies:['html', 'bootstrap', 'Ruby'],
@@ -73,7 +70,7 @@ const projects = [
   },
   {
     projectID:3,
-    name: 'Proffesional art printing data',
+    name: 'Website Protfolio',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     featuredImage: 'images/firstprojectbig.png',
     technologies:['html', 'bootstrap', 'Ruby'],
@@ -83,7 +80,7 @@ const projects = [
   },
   {
     projectID:4,
-    name: 'Proffesional art printing data',
+    name: 'Profesional Art Printing Data More',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     featuredImage: 'images/firstprojectbig.png',
     technologies:['html', 'bootstrap', 'Ruby'],
@@ -93,7 +90,7 @@ const projects = [
   },
   {
     projectID:5,
-    name: 'Proffesional art printing data',
+    name: 'Data Dashboard Healthcare',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     featuredImage: 'images/firstprojectbig.png',
     technologies:['html', 'bootstrap', 'Ruby'],
@@ -103,7 +100,7 @@ const projects = [
   },
   {
     projectID:6,
-    name: 'Proffesional art printing data',
+    name: 'Website Protfolio',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     featuredImage: 'images/firstprojectbig.png',
     technologies:['html', 'bootstrap', 'Ruby'],
@@ -114,90 +111,110 @@ const projects = [
 ];
 
 const dinamicProjects = document.querySelector('#dinamicProject');
+
+//CARDS BEING GENERATED
 for(let index=1 ; index<projects.length; index++){
-//projects.forEach( (project, index)=> {
+  //projects.forEach( (project, index)=> {
   let project = projects[index];
-  //Div projects
-  let divProject=document.createElement('div');
-  divProject.setAttribute("class","projects");
-  //See project button
-   let btnSeeProject = document.createElement('button');
-   let btnText = document.createTextNode('See project');
-   btnSeeProject.setAttribute("class", "buttons neonText2"); 
-   //btnSeeProject.setAttribute("id", `btnSeeProject${project.projectID}`);
-   btnSeeProject.setAttribute("id", project.projectID);
-   btnSeeProject.appendChild(btnText);
-   divProject.appendChild(btnSeeProject);
-   //Technologis ul
-   let techUl = document.createElement('ul');
-   techUl.setAttribute("class","skillsused");
-   let techs = project.technologies;
-   techs.forEach((tech)=>{
-    let liTech = document.createElement('li');
-    let liText = document.createTextNode(tech);
-    liTech.appendChild(liText);
-    techUl.appendChild(liTech);
-    divProject.appendChild(techUl);
-   });
-     //description
-     let descriptionTag = document.createElement('p');
-     let descriptionText = document.createTextNode(project.description);
-     descriptionTag.appendChild(descriptionText);
-     divProject.appendChild(descriptionTag);
-     //title
-     let projectName = document.createElement('h2');
-     let projectNameText = document.createTextNode(project.name);
-     projectName.appendChild(projectNameText);
-     divProject.appendChild(projectName);
+    //Div projects
+    let divProject=document.createElement('div');
+    divProject.setAttribute("class","projects");
+      //See project button
+      let btnSeeProject = document.createElement('button');
+      let btnText = document.createTextNode('See project');
+      btnSeeProject.setAttribute("class", "buttons neonText2"); 
+        //btnSeeProject.setAttribute("id", `btnSeeProject${project.projectID}`);
+        btnSeeProject.setAttribute("id", project.projectID);
+        btnSeeProject.appendChild(btnText);
+        divProject.appendChild(btnSeeProject);
+          //Technologis ul
+          let techUl = document.createElement('ul');
+          techUl.setAttribute("class","skillsused");
+          let techs = project.technologies;
+          techs.forEach((tech)=>{
+            let liTech = document.createElement('li');
+            let liText = document.createTextNode(tech);
+            liTech.appendChild(liText);
+            techUl.appendChild(liTech);
+            divProject.appendChild(techUl);
+          });
+              //description
+              let descriptionTag = document.createElement('p');
+              let descriptionText = document.createTextNode(project.description);
+              descriptionTag.appendChild(descriptionText);
+              divProject.appendChild(descriptionTag);
+                //title
+                let projectName = document.createElement('h2');
+                let projectNameText = document.createTextNode(project.name);
+                projectName.appendChild(projectNameText);
+                divProject.appendChild(projectName);
+
       //Add div to dinamicProjects
       dinamicProjects.appendChild(divProject);
 };
 
 const newModal = document.getElementById('modalID');
 
+//GENERATING MODALS
 function fillModal(ID) {
-    const element = projects[ID];
-    let subTitle = document.createElement('h2');
-    let subTitleText = document.createTextNode(element.name);
-    subTitle.appendChild(subTitleText);
-    newModal.appendChild(subTitle);
-        //Modal Technologies ul
-        let techUl = document.createElement('ul');
-        techUl.setAttribute("class","skillsused");
-        let techs = element.technologies;
-        techs.forEach((tech)=>{
-        let liTech = document.createElement('li');
-        let liText = document.createTextNode(tech);
-        liTech.appendChild(liText);
-        techUl.appendChild(liTech);
-        newModal.appendChild(techUl);
-        });
-            //Div modal (image + description + links)
-            let divModal = document.createElement('div');
-            divModal.setAttribute("class","displayFlexModal");
-            newModal.appendChild(divModal);
-              //Modal Image
-              let imgModal = document.createElement('img');
-              imgModal.setAttribute("src", "images/imageModalBig.png");
-              divModal.appendChild(imgModal);
-                //Modal description
-                let modalDescription = document.createElement('p');
-                let modalDescriptionText = document.createTextNode(element.modalDescription);
-                modalDescription.appendChild(modalDescriptionText);
-                divModal.appendChild(modalDescription);
-                  //Modal Links
-                  let modalLinkLiveVersion = document.createElement('a');
-                  let modalLinkLiveVersionText = document.createTextNode('See Live');
-                  modalLinkLiveVersion.setAttribute('href', element.linkLiveVersion);
-                  modalLinkLiveVersion.setAttribute('class', 'link-live');
-                  let modalLinkSource = document.createElement('a');
-                  let modalLinkSourceText = document.createTextNode('See Source');
-                  modalLinkSource.setAttribute('href', element.linkSource);
-                  modalLinkSource.setAttribute('class', 'link-source');
-                  modalLinkLiveVersion.appendChild(modalLinkLiveVersionText);
-                  modalLinkSource.appendChild(modalLinkSourceText);
-                  divModal.appendChild(modalLinkLiveVersion);
-                  divModal.appendChild(modalLinkSource);
+  const element = projects[ID];
+  let subTitle = document.createElement('h2');
+  let subTitleText = document.createTextNode(element.name);
+  subTitle.appendChild(subTitleText);
+  newModal.appendChild(subTitle);
+    //Modal Technologies ul
+    let techUl = document.createElement('ul');
+    techUl.setAttribute("class","skillsused");
+    let techs = element.technologies;
+    techs.forEach((tech)=>{
+    let liTech = document.createElement('li');
+    let liText = document.createTextNode(tech);
+    liTech.appendChild(liText);
+    techUl.appendChild(liTech);
+    newModal.appendChild(techUl);
+    });
+      //Div modal (image + description + links)
+      let divModal = document.createElement('div');
+      divModal.setAttribute("class","displayFlexModal");
+      newModal.appendChild(divModal);
+        //Modal Image
+        let imgModal = document.createElement('img');
+        imgModal.setAttribute("src", "images/imageModalBig.png");
+        divModal.appendChild(imgModal);
+          //div for p and div(links)
+          let divInner = document.createElement('div');
+          divInner.setAttribute('class', 'divInner');
+          divInner.setAttribute('class', "displayFlexModal2");
+          divModal.appendChild(divInner)
+            //Modal description
+            let modalDescription = document.createElement('p');
+            let modalDescriptionText = document.createTextNode(element.modalDescription);
+            modalDescription.appendChild(modalDescriptionText);
+            divInner.appendChild(modalDescription);
+              //div for links
+              let divLinks = document.createElement('div');
+              divLinks.setAttribute('class', 'divLinks');
+              divInner.appendChild(divLinks)
+                //Modal Links
+                let modalLinkLiveVersion = document.createElement('a');
+                let modalLinkLiveVersionText = document.createTextNode('See Live');
+                modalLinkLiveVersion.setAttribute('href', element.linkLiveVersion);
+                modalLinkLiveVersion.setAttribute('class', 'link-live');
+                let modalLinkSource = document.createElement('a');
+                let modalLinkSourceText = document.createTextNode('See Source');
+                modalLinkSource.setAttribute('href', element.linkSource);
+                modalLinkSource.setAttribute('class', 'link-source');
+                modalLinkLiveVersion.appendChild(modalLinkLiveVersionText);
+                modalLinkSource.appendChild(modalLinkSourceText);
+                divLinks.appendChild(modalLinkLiveVersion);
+                divLinks.appendChild(modalLinkSource);
+                  //close buttom for modals
+                  let closureButtom = document.createElement('button');
+                  closureButtomX = document.createTextNode('X');
+                  closureButtom.setAttribute('id', 'x-button');
+                  closureButtom.setAttribute('class', 'close-buttom');
+                  closureButtom.appendChild(closureButtomX);
+                  divInner.appendChild(closureButtom);
 }
 
 function clearModal(ID) {
@@ -225,5 +242,3 @@ dinamicProjects.addEventListener('click', function (e) {
 });
 
 let btnShowProject=document.querySelector('.buttons');
-
-/*btnShowProject.addEventListener('click', () => showModal(btnShowProject.id));*/
