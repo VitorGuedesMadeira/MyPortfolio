e4491397289cb840d629bfe673ae5b1caf5f0d78
Vitor Guedes <vgm_rox@hotmail.com>
2022-06-16 14:34:00 -0300

    Add contat validation form

--- main.js
+++ main.js
@@ -201,6 +201,7 @@ function fillModal(ID) {
   //  Modal Links
   const modalLinkLiveVersion = document.createElement('a');
   const modalLinkLiveVersionText = document.createTextNode('See Live');
+  const
   modalLinkLiveVersion.setAttribute('href', element.linkLiveVersion);
   modalLinkLiveVersion.setAttribute('class', 'link-live');
   const modalLinkSource = document.createElement('a');
@@ -240,3 +241,64 @@ dinamicProjects.addEventListener('click', (e) => {
     showModal(elementId);
   }
 });
+
+// -----------------------------------------------------------
+
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const userName = document.getElementById('name');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
+}
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+}
+
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email));
+}
+
+const checkLowerCase = email => {
+  if(email === email.lowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const validateInputs = () => {
+  const emailValue = email.value.trim(); //Removes the white spaces
+  const userNameValue = userName.value.trim();
+
+  if(userNameValue === '') {
+      setError(userName, 'Username is required');
+  } else {
+      setSuccess(userName);
+  }
+
+  if(emailValue === '') {
+      setError(email, 'Email is required');
+  } else if (!isValidEmail(emailValue)) {
+      setError(email, 'Provide a valid email adress');
+  } else if (checkLowerCase(emailValue)) { 
+      setError(email, 'Use lower case for the email');
+  } else {
+      setSuccess(email);
+  }
+};
