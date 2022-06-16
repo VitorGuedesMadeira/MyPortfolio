2589b670bb22fbe0d070c1bf594bbb4322b133a6
Vitor Guedes <vgm_rox@hotmail.com>
2022-06-15 21:32:08 -0300

    Fix linters

--- main.js
+++ main.js
@@ -18,26 +18,26 @@ function closeMenu() {
 menubutton.onclick = () => { toggleMenu(); };
 iconsButtons.onclick = () => { closeMenu(); };
 
-//-------------------Modal functions OPEN/CLOSE-----------------//
+//  -------------------Modal functions OPEN/CLOSE-----------------//
 
 function toggleModal(ID) {
   const modal = document.getElementById(ID);
   modal.classList.add('showUp');
 
   window.onclick = function (event) {
-    if (event.target == modal || event.target.className == 'close-buttom') {
+    if (event.target === modal || event.target.className === 'close-buttom') {
       modal.classList.remove('showUp');
     }
-  }
+  };
 
   closeButtom.onclick = function () {
     modal.classList.remove('showUp');
-  }
+  };
 }
 
-//-------------------DATA STORAGE (array of objects)------------------------//
+//  -------------------DATA STORAGE (array of objects)------------------------//
 
-//CARDS DATA
+//  CARDS DATA
 const projects = [
   {
     projectID: 0,
@@ -79,7 +79,7 @@ const projects = [
     technologies: ['html', 'bootstrap', 'Ruby'],
     linkLiveVersion: 'https://www.google.com',
     linkSource: 'https://www.github.com',
-    modalDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.'
+    modalDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
   },
   {
     projectID: 4,
@@ -89,7 +89,7 @@ const projects = [
     technologies: ['html', 'bootstrap', 'Ruby'],
     linkLiveVersion: 'https://www.google.com',
     linkSource: 'https://www.github.com',
-    modalDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.'
+    modalDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
   },
   {
     projectID: 5,
@@ -99,7 +99,7 @@ const projects = [
     technologies: ['html', 'bootstrap', 'Ruby'],
     linkLiveVersion: 'https://www.google.com',
     linkSource: 'https://www.github.com',
-    modalDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.'
+    modalDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
   },
   {
     projectID: 6,
@@ -109,110 +109,110 @@ const projects = [
     technologies: ['html', 'bootstrap', 'Ruby'],
     linkLiveVersion: 'https://www.google.com',
     linkSource: 'https://www.github.com',
-    modalDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.'
-  }
+    modalDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+  },
 ];
 
 const dinamicProjects = document.querySelector('#dinamicProject');
-//CARDS BEING GENERATED
-for(let index = 1; index < projects.length; index++) {
-  //projects.forEach( (project, index)=> {
-  let project = projects[index];
-  //Div projects
-  let divProject = document.createElement('div');
-  divProject.setAttribute("class", "projects");
-  //See project button
-  let btnSeeProject = document.createElement('button');
-  let btnText = document.createTextNode('See project');
-  btnSeeProject.setAttribute("class", "buttons neonText2");
-  //btnSeeProject.setAttribute("id", `btnSeeProject${project.projectID}`);
-  btnSeeProject.setAttribute("id", project.projectID);
+//  CARDS BEING GENERATED
+for (let index = 1; index < projects.length; index += 1) {
+  //  projects.forEach( (project, index)=> {
+  const project = projects[index];
+  //  Div projects
+  const divProject = document.createElement('div');
+  divProject.setAttribute('class', 'projects');
+  //  See project button
+  const btnSeeProject = document.createElement('button');
+  const btnText = document.createTextNode('See project');
+  btnSeeProject.setAttribute('class', 'buttons neonText2');
+  //  btnSeeProject.setAttribute("id", `btnSeeProject${project.projectID}`);
+  btnSeeProject.setAttribute('id', project.projectID);
   btnSeeProject.appendChild(btnText);
   divProject.appendChild(btnSeeProject);
-  //Technologis ul
-  let techUl = document.createElement('ul');
-  techUl.setAttribute("class", "skillsused");
-  let techs = project.technologies;
+  //  Technologis ul
+  const techUl = document.createElement('ul');
+  techUl.setAttribute('class', 'skillsused');
+  const techs = project.technologies;
   techs.forEach((tech) => {
-    let liTech = document.createElement('li');
-    let liText = document.createTextNode(tech);
+    const liTech = document.createElement('li');
+    const liText = document.createTextNode(tech);
     liTech.appendChild(liText);
     techUl.appendChild(liTech);
     divProject.appendChild(techUl);
   });
-  //description
-  let descriptionTag = document.createElement('p');
-  let descriptionText = document.createTextNode(project.description);
+  //  description
+  const descriptionTag = document.createElement('p');
+  const descriptionText = document.createTextNode(project.description);
   descriptionTag.appendChild(descriptionText);
   divProject.appendChild(descriptionTag);
-  //title
-  let projectName = document.createElement('h2');
-  let projectNameText = document.createTextNode(project.name);
+  //  title
+  const projectName = document.createElement('h2');
+  const projectNameText = document.createTextNode(project.name);
   projectName.appendChild(projectNameText);
   divProject.appendChild(projectName);
 
-  //Add div to dinamicProjects
+  //  Add div to dinamicProjects
   dinamicProjects.appendChild(divProject);
-};
+}
 
 const newModal = document.getElementById('modalID');
-//GENERATING MODALS
+//  GENERATING MODALS
 function fillModal(ID) {
   const element = projects[ID];
-  //div for Title and techonolgies
-  let divTitle = document.createElement('div');
+  //  div for Title and techonolgies
+  const divTitle = document.createElement('div');
   divTitle.setAttribute('class', 'divTitle');
-  newModal.appendChild(divTitle)
-  let subTitle = document.createElement('h2');
-  let subTitleText = document.createTextNode(element.name);
+  newModal.appendChild(divTitle);
+  const subTitle = document.createElement('h2');
+  const subTitleText = document.createTextNode(element.name);
   subTitle.appendChild(subTitleText);
   divTitle.appendChild(subTitle);
-  //Modal Technologies ul
-  let techUl = document.createElement('ul');
-  techUl.setAttribute("class", "skillsused");
-  let techs = element.technologies;
+  //  Modal Technologies ul
+  const techUl = document.createElement('ul');
+  techUl.setAttribute('class', 'skillsused');
+  const techs = element.technologies;
   techs.forEach((tech) => {
-    let liTech = document.createElement('li');
-    let liText = document.createTextNode(tech);
+    const liTech = document.createElement('li');
+    const liText = document.createTextNode(tech);
     liTech.appendChild(liText);
     techUl.appendChild(liTech);
     divTitle.appendChild(techUl);
   });
-  //Div modal (image + description + links)
-  let divModal = document.createElement('div');
-  divModal.setAttribute("class", "displayFlexModal");
+  //  Div modal (image + description + links)
+  const divModal = document.createElement('div');
+  divModal.setAttribute('class', 'displayFlexModal');
   newModal.appendChild(divModal);
-  //Modal Image
-  let imgModal = document.createElement('img');
-  imgModal.setAttribute("src", "images/imageModalBig.png");
+  //  Modal Image
+  const imgModal = document.createElement('img');
+  imgModal.setAttribute('src', 'images/imageModalBig.png');
   divModal.appendChild(imgModal);
-  //div for p and div(links)
-  let divInner = document.createElement('div');
-  divInner.setAttribute('class', "displayFlexModal2");
+  //  div for p and div(links)
+  const divInner = document.createElement('div');
+  divInner.setAttribute('class', 'displayFlexModal2');
   divModal.appendChild(divInner);
-  //Modal description
-  let modalDescription = document.createElement('p');
-  let modalDescriptionText = document.createTextNode(element.modalDescription);
+  //  Modal description
+  const modalDescription = document.createElement('p');
+  const modalDescriptionText = document.createTextNode(element.modalDescription);
   modalDescription.appendChild(modalDescriptionText);
   divInner.appendChild(modalDescription);
-  //div for links
-  let divLinks = document.createElement('div');
+  //  div for links
+  const divLinks = document.createElement('div');
   divLinks.setAttribute('class', 'divLinks');
   divInner.appendChild(divLinks);
-  //Modal Links
-  let modalLinkLiveVersion = document.createElement('a');
-  let modalLinkLiveVersionText = document.createTextNode('See Live');
+  //  Modal Links
+  const modalLinkLiveVersion = document.createElement('a');
+  const modalLinkLiveVersionText = document.createTextNode('See Live');
   modalLinkLiveVersion.setAttribute('href', element.linkLiveVersion);
   modalLinkLiveVersion.setAttribute('class', 'link-live');
-  let modalLinkSource = document.createElement('a');
-  let modalLinkSourceText = document.createTextNode('See Source');
+  const modalLinkSource = document.createElement('a');
+  const modalLinkSourceText = document.createTextNode('See Source');
   modalLinkSource.setAttribute('href', element.linkSource);
   modalLinkSource.setAttribute('class', 'link-source');
   modalLinkLiveVersion.appendChild(modalLinkLiveVersionText);
   modalLinkSource.appendChild(modalLinkSourceText);
   divLinks.appendChild(modalLinkLiveVersion);
   divLinks.appendChild(modalLinkSource);
-  //close buttom for modals
+  //  close buttom for modals
   const closureButtom = document.createElement('button');
   const closureButtomX = document.createTextNode('X');
   closureButtom.setAttribute('id', 'x-button');
@@ -234,7 +234,7 @@ function showModal(ID) {
   toggleModal('modal-containerID0');
 }
 
-dinamicProjects.addEventListener('click', function (e) {
+dinamicProjects.addEventListener('click', (e) => {
   const elementId = e.target.id;
   // But only alert for elements that have an buttons class
   if (e.target.classList.contains('buttons')) {
